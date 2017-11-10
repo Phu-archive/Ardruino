15c3fb3c073e2d5cf7c352aafc84a4d446545527
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2017-11-10 20:33:51 +0000

    Adding Comments

--- GoArduino/GoArduino.ino
+++ GoArduino/GoArduino.ino
@@ -1,56 +1,61 @@
+// Define all the buttons pin
 const int buttonPin1 = 8;
-const int buttonPin2 = 9;     
-const int buttonPin3 = 11; 
-const int buttonPin4 = 12; 
-const int buttonPin5 = 13; 
+const int buttonPin2 = 9;
+const int buttonPin3 = 11;
+const int buttonPin4 = 12;
+const int buttonPin5 = 13;
 
-int count = 0;
-
+// Initialize all the buttons states
 int button1State = 0;
 int button2State = 0;
 int button3State = 0;
 int button4State = 0;
 int button5State = 0;
 
-int LDRValue = 0;
-
 void setup() {
   Serial.begin(9600);
-  pinMode(buttonPin1, INPUT_PULLUP);   
-  pinMode(buttonPin2, INPUT_PULLUP);   
-  pinMode(buttonPin3, INPUT_PULLUP); 
-  pinMode(buttonPin4, INPUT_PULLUP); 
-  pinMode(buttonPin5, INPUT_PULLUP); 
+
+  // Define the pin modes.
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(buttonPin3, INPUT_PULLUP);
+  pinMode(buttonPin4, INPUT_PULLUP);
+  pinMode(buttonPin5, INPUT_PULLUP);
 }
 
 void loop(){
+  // Read the pin value from arduino
   button1State = digitalRead(buttonPin1);
   button2State = digitalRead(buttonPin2);
   button3State = digitalRead(buttonPin3);
   button4State = digitalRead(buttonPin4);
   button5State = digitalRead(buttonPin5);
-      if (button1State == HIGH) { 
-        Serial.write(1);
-      }
-      else if(button2State == HIGH){
-        Serial.write(2);
-      }
-      else if(button3State == HIGH){
-        Serial.write(3);
-      }
-      else if(button4State == HIGH){
-        Serial.write(4);
-      }
-      else if(button5State == HIGH){
-        Serial.write(5);
-      } else {
-        Serial.write(0);
-      }
-  
-   
 
-    
-  
+  // If the button x is high then write the number to the serial port
+  // Else write zero.
+  if (button1State == HIGH) {
+    Serial.write(1);
+  }
+  else if(button2State == HIGH){
+    Serial.write(2);
+  }
+  else if(button3State == HIGH){
+        Serial.write(3);
+  }
+  else if(button4State == HIGH){
+    Serial.write(4);
+  }
+  else if(button5State == HIGH){
+    Serial.write(5);
+  }
+  else {
+    Serial.write(0);
+  }
+
+
+
+
+
   // Wait for 0.5 seconds
   delay(100);
 }
--- PongArduino/Pong.ino
+++ PongArduino/Pong.ino
@@ -1,55 +1,66 @@
+// Define all the buttons pin
+// And LRD pin
 const int buttonPin1 = 8;
-const int buttonPin2 = 9;     
+const int buttonPin2 = 9;
 const int LDRPin = A1;
-const int buttonPin3 = 11; 
-const int buttonPin4 = 12; 
-const int buttonPin5 = 13; 
+const int buttonPin3 = 11;
+const int buttonPin4 = 12;
+const int buttonPin5 = 13;
 
+// Initialize all the buttons states
 int button1State = 0;
 int button2State = 0;
 int button3State = 0;
 int button4State = 0;
 int button5State = 0;
 
+// Initialize all the LDR value
 int LDRValue = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(buttonPin1, INPUT_PULLUP);   
-  pinMode(buttonPin2, INPUT_PULLUP);   
-  pinMode(buttonPin3, INPUT_PULLUP); 
-  pinMode(buttonPin4, INPUT_PULLUP); 
-  pinMode(buttonPin5, INPUT_PULLUP); 
+
+  // Define the pin modes.
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(buttonPin3, INPUT_PULLUP);
+  pinMode(buttonPin4, INPUT_PULLUP);
+  pinMode(buttonPin5, INPUT_PULLUP);
 }
 
 void loop(){
+  // Read the pin value from arduino
   button1State = digitalRead(buttonPin1);
   button2State = digitalRead(buttonPin2);
   button3State = digitalRead(buttonPin3);
   button4State = digitalRead(buttonPin4);
   button5State = digitalRead(buttonPin5);
+
+  // Read the LDR value.
   LDRValue = analogRead(LDRPin);
 
-      if (button1State == HIGH) { 
-        Serial.write(1);
-      }
-      else if(button2State == HIGH){
-        Serial.write(2);
-      }
-      else if(button3State == HIGH){
-        Serial.write(3);
-      }
-      else if(button4State == HIGH){
-        Serial.write(4);
-      }
-      else if(button5State == HIGH){
-        Serial.write(5);
-      } else {
-        Serial.write(0);
-      }
+  // If the button x is high then write the number to the serial port
+  // Else write the LRD value.
+  if (button1State == HIGH) {
+    Serial.write(1);
+  }
+  else if(button2State == HIGH){
+    Serial.write(2);
+  }
+  else if(button3State == HIGH){
+    Serial.write(3);
+  }
+  else if(button4State == HIGH){
+    Serial.write(4);
+  }
+  else if(button5State == HIGH){
+    Serial.write(5);
+  } else {
+    Serial.write(0);
+  }
 
-      Serial.write(LDRValue);
+  Serial.write(LDRValue);
 
-  // Wait for 0.5 seconds
+  // Adding the delay so that there won't be too much information flow.
   delay(100);
 }
